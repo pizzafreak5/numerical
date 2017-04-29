2ea0f71bc20813561097fba2fa67598bf713b59d
Garrett <pf5dev@gmail.com>
2017-04-28 18:08:14 -0600

    lol

--- integrator.py
+++ integrator.py
@@ -4,11 +4,11 @@
 
 #Originaly Written By Garrett Schwartz, 04-24-2017
 
-import Tkinter
 from math import *
 import re
 import parser
 import numpy
+import compiler #For gaussian
 
 
 
@@ -164,50 +164,57 @@ def adaptive_trap(function, start, end, tolerance):
     tol.append(tolerance)
 
     a.append(float(start))
-    b.append(float(start))
+    b.append(float(end))
+
+    result = 0.0
     
     while n >= 0:
+
         c = (a[n] + b[n]) / float(2)
         old_app = app[n]
 
-        if len(app) < n:
+        if len(app) <= n:
             app.append(float('nan'))
 
         app[n] = trap(func, a[n], c)
 
-        if len(app) < n + 1:
+        if len(app) <= n + 1:
             app.append(float('nan'))
 
-        app[n+1] = trap(function, c, b[n])
+        app[n+1] = trap(func, c, b[n])
+        
 
-        if abs(oldapp - (app[n] + app[n+1])) < 3*tol[n]:
+        if abs(old_app - (app[n] + app[n+1])) < 3*tol[n]:
+            
             result += app[n] + app[n+1]
             n = n-1
         else:
-            if len(b) < n:
+            if len(b) <= n:
                 b.append(float('nan'))
 
-            if len(a) < n:
+            if len(a) <= n:
                 a.append(float('nan'))
 
-            if len(b) < n+1:
+            if len(b) <= n+1:
                 b.append(float('nan'))
 
-            if len(a) < n+1:
+            if len(a) <= n+1:
                 a.append(float('nan'))
 
-            b[n+1] = b[n+1] = b(n)
+            b[n+1] = b[n]
             b[n] = c
             a[n+1] = c
 
-            tol[n] = tol[n] / 2
+            tol[n] = tol[n] / float(2)
 
-            if len(tol) < n+1:
+            if len(tol) <= n+1:
                 tol.append(float('nan'))
 
             tol[n+1] = tol[n]
 
             n += 1
+
+    return result
 
         
         
@@ -278,8 +285,89 @@ def composite_midpoint_rule(f, a, b, m):
 
 
 #===================================#
-#       Composite Midpoint          #
+#       Gaussian Quadrature         #
 #===================================#
+
+#references https://rosettacode.org/wiki/Numerical_integration/Gauss-Legendre_Quadrature#Python
+
+def gaussian_quadrature(function, polyorder, a, b):
+    func_string = 'def temp_func(x):\n\treturn ' + function
+    h = parser.suite(func_string).compile()
+    exec(h)
+    
+
+    [Ws, xs, err] = GaussLegendreWeights(polyorder)
+
+    if err == 0:
+        ans = (b-a) * 0.5 * sum(Ws * temp_func((b-a)*0.5*xs + (b+a)*0.5))
+
+    else:
+        err = 1
+        ans = None
+    return [ans, err]
+
+#===================================#
+#       Gaussian Utilities          #
+#===================================#
+
+def legendre(n, x):
+
+    x = numpy.array(x)
+
+    if n == 0:
+        return x*0+1.0 #addition of x*0 is most likely for correct datatype
+    elif n == 1:
+        return x
+    else:
+        return ((2.0 * n - 1.0) * x * legendre(n-1, x) - (n-1) * legendre(n-2, x))/n
+
+def dlegendre(n, x):
+
+    x = numpy.array(x)
+    if n == 0 :
+        return x * 0
+    elif n == 1:
+        return x*0+1.0
+    else:
+        return (n / (x**2 - 1.0))*(x*legendre(n,x) - legendre(n-1, x))
+
+def legendre_roots(polyorder, tolerance = 1e-20):
+    if polyorder < 2:
+        err = 1 #bad poly order
+
+    else:
+        roots = []
+
+        for i in range(1, int(polyorder)/2 + 1):
+            x = cos(pi * (i - 0.25)/(polyorder+0.5))
+            error = 10 * tolerance
+            iters = 0
+            while error > tolerance and iters < 1000:
+                dx = -legendre(polyorder, x) / dlegendre(polyorder, x)
+                x = x+dx
+                iters = iters + 1
+                error = abs(dx)
+            roots.append(x)
+
+        roots = numpy.array(roots)
+        if polyorder%2 == 0:
+            roots = numpy.concatenate((-1.0*roots, roots[::-1]))
+        else:
+            roots = numpy.concatenate((-1.0*roots, [0.0], roots[::-1]))
+        err = 0
+    return [roots, err]
+
+def GaussLegendreWeights(polyorder):
+    w = []
+    [xis, err] = legendre_roots(polyorder)
+    if err == 0:
+        w = 2.0/((1.0 - xis**2) * dlegendre(polyorder, xis)**2)
+        err = 0
+    else:
+        err=1
+    return [w, xis, err]
+            
+
 
 #===================================#
 #               Utility             #
@@ -288,7 +376,7 @@ def composite_midpoint_rule(f, a, b, m):
 def trap(compiled_function, a, b):
     floata = float(a)
     floatb = float(b)
-    return trap_area(eval_function(compiled_function, a), eval_function(compiled_function, b), (floatb - floata)/2)
+    return trap_area(eval_function(compiled_function, a), eval_function(compiled_function, b), (floatb - floata))
 
 #NOTE: Rewrite to move compilation out of function
          
